40ec87ec4b62fa3f5191d95678a0bbfa125303cc
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-28 09:36:36 -0500

    one

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function
+def close10(x,y):
+    if abs(x - 10)<abs(y - 10):
+        return x
+
+    elif abs(x - 10)>abs(y-10):
+        return y
+
+    else:
+        return 0
+
+
+        
